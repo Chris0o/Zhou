e083e852d9ea345609813a7df7ed3d3ca624dbc0
zhouhongjun_loc <a857616984@163.com>
2019-02-14 10:47:58 +0800

    "修改的代码"

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -4,8 +4,7 @@
 
 public class HelloWorld {
 
-    public static void main(String[] args) {
-
-        System.out.println("Hello,World");
+    public void  test(){
+        System.out.println("修改代码");
     }
 }
